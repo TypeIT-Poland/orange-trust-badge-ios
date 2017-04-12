b91b3b33d07eeded246adbdbc362957256340279
Pawellukasiak <pawel.lukasiak92@gmail.com>
2017-04-12 11:51:39 +0200

    Changed project views

--- OrangeTrustBadge/Classes/UI/Landing/LandingController.swift
+++ OrangeTrustBadge/Classes/UI/Landing/LandingController.swift
@@ -86,7 +86,7 @@ class LandingController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 2
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
